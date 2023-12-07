fd07083f5470b3c8097724f72feefbf6a86d0a17
Manuel Meneses <manuelmenesesg@gmail.com>
2023-12-07 15:58:02 -0400

    chore: setting up eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: true,
+    htmlWhitespaceSensitivity: "css",
+    insertPragma: true,
+    jsxSingleQuote: false,
+    proseWrap: "preserve",
+    quoteProps: "as-needed",
+    requirePragma: true,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: false,
+    printWidth: 200
+  }
+  
